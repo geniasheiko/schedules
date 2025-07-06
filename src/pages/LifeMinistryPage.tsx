b561ab1cac751bdb779311c8ea5ced80422707ca
Egor Melnik <melnikeg@ukr.net>
2025-07-06 19:29:40 +0300

    LifeMinistryPage. Some styles and html.

--- src/pages/LifeMinistryPage.tsx
+++ src/pages/LifeMinistryPage.tsx
@@ -1,9 +1,20 @@
-
+import styles from "./LifeMinistryPage.module.css";
 
 export const LifeMinistryPage = () => {
     return (
-    <div>
-        <h1>Life Ministry</h1>
+    <div className={styles.container}>
+        <h1 className={styles.title}>
+            <span>ЗБІР "ТРУСКАВЕЦЬ"</span>
+            <span>Розклад зібрань серед тижня</span>
+        </h1>
+        <div className={styles.weeksContainer}>
+            <div className={styles.weeksItem}>
+                
+            </div>
+            <div className={styles.weeksItem}></div>
+            <div className={styles.weeksItem}></div>
+            <div className={styles.weeksItem}></div>
+        </div>
     </div>
     )
 }
