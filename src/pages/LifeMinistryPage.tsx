83dac3164cdb218f8dcc7a8a2a39215dd4ca42dc
Egor Melnik <melnikeg@ukr.net>
2025-07-06 22:17:02 +0300

    LifeMinistryPage. Styles and html.

--- src/pages/LifeMinistryPage.tsx
+++ src/pages/LifeMinistryPage.tsx
@@ -3,17 +3,137 @@ import styles from "./LifeMinistryPage.module.css";
 export const LifeMinistryPage = () => {
     return (
     <div className={styles.container}>
-        <h1 className={styles.title}>
-            <span>ЗБІР "ТРУСКАВЕЦЬ"</span>
-            <span>Розклад зібрань серед тижня</span>
-        </h1>
+        <header className={styles.header}>
+            <h1 className={styles.title}>ЗБІР "ТРУСКАВЕЦЬ"</h1>
+            <h2 className={styles.subtitle}>Розклад зібрань серед тижня</h2>
+        </header>
         <div className={styles.weeksContainer}>
-            <div className={styles.weeksItem}>
-                
-            </div>
-            <div className={styles.weeksItem}></div>
-            <div className={styles.weeksItem}></div>
-            <div className={styles.weeksItem}></div>
+            <section className={styles.weeksItem}>
+                <h2 className={styles.weekTitle}>30 ЛИПНЯ 2025</h2>
+                <div className={styles.weekContent}>
+                    <div className={styles.topBlock}>
+                        <div className={styles.chairman}>
+                            <span className={styles.personRole}>Ведучий, молитва:</span>
+                            <span className={styles.person}>Філатов В.</span>
+                        </div>
+                        <div className={styles.introductionConclusion}>
+                            <span className={styles.time}>19:00</span>
+                            <span>Пісня і молитва  |  Вступні слова </span>
+                            <span className={styles.introductionConclusionTime}>(1 хв)</span>
+                        </div>
+                    </div>
+                    <div className={styles.treasuresBlock}>
+                        <div className={styles.treasuresTitle}>
+                            <img className={styles.treasuresImg} src="public/treasures.png" alt="Treasures" />
+                            <h3 className={styles.treasuresText}>СКАРБИ З БОЖОГО СЛОВА</h3>
+                        </div>
+                        <ul className={styles.treasuresList}>
+                            <li className={styles.treasuresItem}>
+                                <span className={styles.time}>19:06</span>
+                                <span className={styles.treasuresNumber}>1.</span>
+                                <span className={styles.person}>Бондаренко В.</span>
+                            </li>
+                            <li className={styles.treasuresItem}>
+                                <span className={styles.time}>19:16</span>
+                                <span className={styles.treasuresNumber}>2.</span>
+                                <span className={styles.person}>Мельник Є.</span>
+                            </li>
+                            <li className={styles.treasuresItem}>
+                                <span className={styles.time}>19:26</span>
+                                <span className={styles.treasuresNumber}>3.</span>
+                                <span className={styles.person}>Козлюк Ю.</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.ministryBlock}>
+                        <div className={styles.ministryTitle}>
+                            <img className={styles.ministryImg} src="public/apply.png" alt="Ministry" />
+                            <h3 className={styles.ministryText}>ВДОСКОНАЛЮЙМО СВОЄ СЛУЖІННЯ</h3>
+                        </div>
+                        <ul className={styles.ministryList}>
+                            <li className={styles.ministryItem}>
+                                <span className={styles.time}>19:31</span>
+                                <span className={styles.ministryNumber}>4.</span>
+                                <span className={styles.person}>Кобзар Г. / Рихлінська Г.</span>
+                            </li>
+                            <li className={styles.ministryItem}>                                
+                                <span className={styles.time}>19:34</span>
+                                <span className={styles.ministryNumber}>5.</span>
+                                <span className={styles.person}>Липак М. / Рихлінська М.</span>
+                            </li>
+                            <li className={styles.ministryItem}>
+                                <span className={styles.time}>19:38</span>
+                                <span className={styles.ministryNumber}>6.</span>
+                                <span className={styles.person}>Мельник Єв. / Залокоцька О.</span>
+                            </li>
+                            <li className={styles.ministryItem}>
+                                <span className={styles.time}>19:42</span>
+                                <span className={styles.ministryNumber}>7.</span>
+                                <span className={styles.person}>Маяков В.</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.lifeBlock}>
+                        <div className={styles.lifeTitle}>
+                            <img className={styles.lifeImg} src="public/living.png" alt="Ministry" />
+                            <h3 className={styles.lifeText}>ХРИСТИЯНСЬКЕ ЖИТТЯ</h3>
+                        </div>
+                        <div className={styles.middleBlock}>
+                            <span className={styles.time}>19:46</span><span>Пісня</span>
+                        </div>
+                        <ul className={styles.lifeList}>
+                            <li className={styles.lifeItem}>
+                                <span className={styles.time}>19:51</span>
+                                <span className={styles.lifeNumber}>8.</span>
+                                <span className={styles.person}>Мельник Є.</span>
+                            </li>
+                            <li className={styles.lifeItem}>
+                                <span className={styles.time}>20:06</span>
+                                <span className={styles.lifeNumber}>9.</span>
+                                <span className={styles.person}>Мураль В. / Паталаха М.</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.bottomBlock}>
+                        <div className={styles.introductionConclusion}>Кінцеві слова <span className={styles.introductionConclusionTime}>(3 хв)</span>  |  Пісня і молитва</div>
+                        <div className={styles.lastItem}>
+                            <span className={styles.personRole}>Молитва:</span>
+                            <span className={styles.person}>Паталаха М.</span>
+                        </div>
+                    </div>
+                </div>
+                <footer className={styles.weekFooter}>
+                    <div className={`${styles.footerItem} ${styles.service}`}>
+                        <h3 className={styles.footerTitle}>ОБСЛУГОВУВАННЯ</h3>
+                        <ul className={styles.footerList}>
+                            <li className={styles.footerItem}>
+                                <span className={styles.personRole}>Мікшер | Сцена:</span>
+                                <span className={styles.person}>Залокоцький Н. | Дзюбанов А.</span>
+                            </li>
+                            <li className={styles.footerItem}>
+                                <span className={styles.personRole}>Обслуговуючі:</span>
+                                <span className={styles.person}>Паталаха М. | Кондаревич В.</span>
+                            </li>
+                            <li className={styles.footerItem}>
+                                <span className={styles.personRole}>Мікрофони:</span>
+                                <span className={styles.person}>Сворень Я. | Салань А.</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={`${styles.footerItem} ${styles.cleaning}`}>
+                        <h3 className={styles.footerTitle}>ПРИБИРАННЯ</h3>
+                        <div className={styles.footerList}>
+                            <div className={styles.footerItem}>
+                                <span className={styles.personRole}>Група</span>
+                                <span className={styles.person}>1</span>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </section>
+            <section className={styles.weeksItem}></section>
+            <section className={styles.weeksItem}></section>
+            <section className={styles.weeksItem}></section>
         </div>
     </div>
     )
