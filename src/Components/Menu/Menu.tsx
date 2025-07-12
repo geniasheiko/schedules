6b632097fa08258ab77b173183ae2b4bc5cc5aa4
Egor Melnik <melnikeg@ukr.net>
2025-07-12 19:30:47 +0300

    Top menu changes.

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -5,10 +5,10 @@ export const Menu = () => {
     return (
         <nav className={styles.menu}>
             <Link to="/" className={styles.link}>Запис на стенд</Link>
-            <Link to="/life-and-ministry" className={styles.link}>Зібрання серед тижня</Link>
+            <Link to="/life-and-ministry" className={styles.link}>НХЖС</Link>
+            <Link to="/lists" className={styles.link}>Списки</Link>
             <Link to="/public-talk-watchtower" className={styles.link}>Зібрання на вихідних</Link>
             <Link to="/meetings-for-field-service" className={styles.link}>Зустрічі для служіння</Link>
-            <Link to="/lists" className={styles.link}>Списки</Link>
         </nav>
     )
 }
