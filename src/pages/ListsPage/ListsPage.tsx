64858b3e09905097ba033a0d717bdc503dea0298
Egor Melnik <melnikeg@ukr.net>
2025-10-26 20:49:53 +0200

    Frames with meeting schedules and lists have been temporarily added

--- src/pages/ListsPage/ListsPage.tsx
+++ src/pages/ListsPage/ListsPage.tsx
@@ -6,8 +6,19 @@ export const ListsPage = () => {
         <header className={styles.headerTitle}>
             <h1 className={styles.title}>ЗБІР "ТРУСКАВЕЦЬ"</h1>
         </header>
-        <h2 className={styles.subtitle}>Список по групах</h2>
-        <h2 className={styles.subtitle}>Список вісників, схвалених для служіння зі стендом</h2>
+        <div className={styles.container}>
+            <div className={styles.iframeContainer}>
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTjFaAVPtpFcupjHQ9Z00YuydqAFBUmqvYIKPi2xCIt20Je4ecwBmCKJ7We35inZrol4efN6r31dK8B/pubhtml?gid=606116683&amp;single=true&amp;widget=true&amp;headers=false"
+                    width="100%"
+                    height="1200"
+                    frameBorder="0"
+                ></iframe>
+            </div>
+        </div>
+        <div className={styles.temp}>
+            <h2 className={styles.subtitle}>Список по групах</h2>
+            <h2 className={styles.subtitle}>Список вісників, схвалених для служіння зі стендом</h2>
+        </div>
     </div>
     )
 }
